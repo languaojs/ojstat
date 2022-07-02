291f78a96419df9004dc34626f3c86a84fc0912a
languaojs <107660406+languaojs@users.noreply.github.com>
2022-07-03 01:58:16 +0700

    Create error.php

---
+++ app/views/online/error.php
@@ -0,0 +1,10 @@
+<div class="container">
+    <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+            <p class="bg-danger text-light d-flex justify-content-center align-items-center py-2">
+                <span class="display-4 mr-2"><i class="fa fa-warning"></i> ERROR</span>
+                <span>Unknown<br>Parameter</span>
+            </p>
+        </div>
+    </div>
+</div>
