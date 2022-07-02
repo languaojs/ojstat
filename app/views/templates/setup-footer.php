31b1055230185a1c313ede0b5e90e58da6b0609c
languaojs <107660406+languaojs@users.noreply.github.com>
2022-07-03 02:01:30 +0700

    Create setup-footer.php

---
+++ app/views/templates/setup-footer.php
@@ -0,0 +1,9 @@
+<div class="container">
+    <div class="row mt-3">
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+            <p>Copyright (c) 2022-<?php echo date('Y');?> OJStat - Coding Alone Project</p>
+        </div>
+    </div>
+</div>
+</body>
+</html>
