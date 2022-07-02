0774713ec8c8925788ee7b4272273bcf018b24ec
languaojs <107660406+languaojs@users.noreply.github.com>
2022-07-03 01:59:46 +0700

    Create restriction.php

---
+++ app/views/setup/restriction.php
@@ -0,0 +1,13 @@
+<div class="container">
+    <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+            <h1 class="mt-3"><i class="fa fa-warning text-danger"></i> Access Prohibited</h1>
+            <br>
+            <div class="alert alert-danger">
+                <p>You are not allowed to access OJStat Setup Page. If this is not supposed to happen, please contact your administrator.</p>
+            </div>
+            <br>
+            <p><a href="<?=BASEURL;?>/public/home"><i class="fa fa-arrow-left"></i> Return</a></p>
+        </div>
+    </div>
+</div>
