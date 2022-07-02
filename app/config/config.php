4319927454be8b85eb03b077fcb4da1461999543
languaojs <107660406+languaojs@users.noreply.github.com>
2022-07-03 01:52:18 +0700

    configuration file
    Created to set the baseurl, database information, and setup.

---
+++ app/config/config.php
@@ -0,0 +1,18 @@
+<?php 
+
+//baseurl
+define('BASEURL', ''); //the URL of your OJStat
+
+//database
+define('DB_HOST', ''); // database host name
+define('DB_USER', ''); // database user name
+define('DB_PASS', ''); //database password
+define('DB_NAME', ''); // database name
+
+//setup
+define('SETUP', true); // true if setup is about to run, false if setup has been finished
+define('VERSION', '1.4'); // the current version of OJStat
+define('OJSTATART', true); // true if you want to display OJStat news and false if you do not want it to show
+
+?>
+
