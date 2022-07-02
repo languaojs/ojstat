dc733fc3656b29293fa477bb2a616adc74e47e30
languaojs <107660406+languaojs@users.noreply.github.com>
2022-07-03 02:03:51 +0700

    Create table-system.php

---
+++ app/views/widget/table-system.php
@@ -0,0 +1,44 @@
+<div class="container">
+    <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+            <table class="table table-condensed table-hover table-sm" id="system" title="OJStat | Operating System Widget">
+                <thead>
+                    <tr class="bg-dark text-light">
+                        <th>Operating Systems</th>
+                        <th class='text-center'>All Visitors</th>
+                        <th class='text-center'>Unique Visitors</th>
+                        <th class='text-center'>Returning Visitors</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php 
+                        foreach($data['data'] as $tbl)
+                        {
+                            $icons = "<img src='".BASEURL."/public/img/icons/".strtolower($tbl['pv_os']).".png' width='30' height='30'/>";
+                            echo "<tr>";
+                            echo "<td>";
+                            echo $icons ."  ". Sanitize::clean($tbl['pv_os']);
+                            echo "<td class='text-center'>";
+                            echo Sanitize::clean($tbl['allVisitor']);
+                            echo "<td class='text-center'>";
+                            echo Sanitize::clean($tbl['uniqueVisitor']);
+                            echo "<td class='text-center'>";
+                            echo Sanitize::clean($tbl['allVisitor']-$tbl['uniqueVisitor']);
+                            echo "</td>";
+                            echo "</tr>";
+                        }
+                    ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+<script>
+    $(document).ready(function(){
+        $('#system').dataTable({
+            order:[[1,'desc']],
+            lengthMenu:[[5, 10, 25, 50, 75, 100, -1], [5, 10, 25, 50, 75, 100, "All"]],
+            info:false
+        });
+    });
+</script>
