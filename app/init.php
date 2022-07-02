58be9da75b68fe3dbb93d8834ac3445bed5c5b85
languaojs <107660406+languaojs@users.noreply.github.com>
2022-07-03 01:55:59 +0700

    Create init.php

---
+++ app/init.php
@@ -0,0 +1,8 @@
+<?php 
+require 'core/App.php';
+require 'core/Controller.php';
+require 'core/Sanitize.php';
+require 'core/Database.php';
+require 'core/Session.php';
+require 'core/Flasher.php';
+require 'config/config.php';
