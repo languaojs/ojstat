c1113232724ab1a0c0d53920b1fa53acdba1f4e1
languaojs <107660406+languaojs@users.noreply.github.com>
2022-07-03 02:02:13 +0700

    Create 404.php

---
+++ app/views/widget/404.php
@@ -0,0 +1,11 @@
+<div class="container">
+    <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+            <div id="chartBox" style="width:100%; height:300px" class="p-4 shadow">
+            <h1><i class="fa fa-warning text-danger"></i> 404</h1>
+                <h3>The widget you are looking for does not exist!</h3>
+                <p>This usually happens when wrong parameters are inserted into the iframe source. Check your widget parameters.</p>
+            </div>
+        </div>
+    </div>
+</div>
