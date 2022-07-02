c38b33987f92a10b1e0e2eec59f7b17df922f583
languaojs <107660406+languaojs@users.noreply.github.com>
2022-07-03 02:01:18 +0700

    Create online-header.php

---
+++ app/views/templates/online-header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?= BASEURL;?>/public/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?= BASEURL;?>/public/css/ojstat.css">
+    <script src="<?= BASEURL;?>/public/js/jquery.min.js"></script>
+    <script src="<?= BASEURL;?>/public/js/bootstrap.min.js"></script>
+    <title><?=$data['title'];?></title>
+</head>
+<body>
+
+    
+    
