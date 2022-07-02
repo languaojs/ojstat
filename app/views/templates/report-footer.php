1b4542e8520b1d21fb0793c920d96652f026414a
languaojs <107660406+languaojs@users.noreply.github.com>
2022-07-03 02:01:23 +0700

    Create report-footer.php

---
+++ app/views/templates/report-footer.php
@@ -0,0 +1,9 @@
+<div class="row bg-unique text-light mt-4">
+    <div class="container">
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 py-3 text-center">
+            <p>This report is generated with OJStat version 1.4</p>
+        </div>
+    </div>
+</div>
+</body>
+</html>
