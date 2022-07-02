f5e451478fcd8e448178fd981aabdb0aa2a53162
languaojs <107660406+languaojs@users.noreply.github.com>
2022-07-03 02:01:42 +0700

    Create widget_header.php

---
+++ app/views/templates/widget_header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?= BASEURL;?>/public/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?= BASEURL;?>/public/css/jquery-ui.min.css">
+    <link rel="stylesheet" href="<?= BASEURL;?>/public/css/ojstat.css">
+    <link rel="stylesheet" href="<?= BASEURL;?>/public/css/datatables.min.css">
+    <link rel="stylesheet" href="<?= BASEURL;?>/public/css/fa.all.min.css">
+    <script src="<?= BASEURL;?>/public/js/jquery.min.js"></script>
+    <script src="<?= BASEURL;?>/public/js/bootstrap.min.js"></script>
+    <script src="<?= BASEURL;?>/public/js/jquery-ui.min.js"></script>
+    <script src="<?= BASEURL;?>/public/js/datatables.min.js"></script>
+    <script src="<?= BASEURL;?>/public/js/fa.all.min.js"></script>
+    <script src="<?= BASEURL;?>/public/js/chart.min.js"></script>
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+</head>
+<body>
+
+    
+    
