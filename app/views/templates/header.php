c7308132e8e0f406df0c9a19354194d450ac524f
languaojs <107660406+languaojs@users.noreply.github.com>
2022-07-03 02:01:02 +0700

    Create header.php

---
+++ app/views/templates/header.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?= BASEURL;?>/public/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?= BASEURL;?>/public/css/jquery-ui.min.css">
+    <link rel="stylesheet" href="<?= BASEURL;?>/public/css/ojstat.css">
+    <link rel="stylesheet" href="<?= BASEURL;?>/public/css/datatables.min.css">
+    <link rel="stylesheet" href="<?= BASEURL;?>/public/css/fa.all.min.css">
+    <script src="<?= BASEURL;?>/public/js/jquery.min.js"></script>
+    <script src="<?= BASEURL;?>/public/js/popper.js"></script>
+    <script src="<?= BASEURL;?>/public/js/bootstrap.min.js"></script>
+    <script src="<?= BASEURL;?>/public/js/jquery-ui.min.js"></script>
+    <script src="<?= BASEURL;?>/public/js/datatables.min.js"></script>
+    <script src="<?= BASEURL;?>/public/js/fa.all.min.js"></script>
+    <script src="<?= BASEURL;?>/public/js/chart.min.js"></script>
+    <script src="<?= BASEURL;?>/public/js/dashboard.js"></script>
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <title><?=$data['title'];?></title>
+</head>
+<body>
+<div class="container">
+    <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 flex-between">
+            <div class="p-4">
+                <img src="<?=BASEURL;?>/public/img/logos/ojstat-logo-official.png" alt="" width="150" height="50">
+             </div>
+             <div class="p-4">
+                <i class="text-muted">Version <?=VERSION;?></i>
+            </div>
+        </div>
+    </div>
+</div>
+    
+    
